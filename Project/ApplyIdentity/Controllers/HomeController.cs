0e1ae24c2c7aa560e214d07f617187a5880a8768
Anton Lygin <lygin@live.com>
2016-11-19 23:33:12 +0300

    v0.001 - initial commit (not refined 2012);

---
+++ Project/ApplyIdentity/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MvcState.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data.Add("Placeholder", "Placeholder");
+            return View("~/Views/Shared/Many.cshtml", (object)data);
+        }
+    }
+}
